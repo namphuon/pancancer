e980b3bc24dd1cd69df22a14ef6ca5806105bc98
Nam-phuong Nguyen <namphuon@notifier@genomemaster.ucsd.edu>
2018-05-03 14:21:03 -0700

    Updated cycle code

--- python/find_cycles.py
+++ python/find_cycles.py
@@ -1,13 +1,82 @@
 import hg19util as hg19
 import sys, os
+from sets import Set
+
+#Amplicon classification - Cyclic (has at least one valid cycle)
+#                          Complex (has a path that crosses multiple intervals)
+#                          Simple (all paths stay within the same amplified region)
+def classify_amplicon(amplicon, threshold_copy = 4, min_length = 50000):
+  #Check for cyclic cycle, no threshold
+  intervals = hg19.interval_list([i for i in amplicon['interval_map'].values()])
+  intervals.sort()  
+  cycles = amplicon['cycle_map'].keys()
+  cycles = sorted(cycles, key=lambda x: amplicon['cycle_map'][x]['copy_count'], reverse=True)  
+  iscycle = False
+  iscomplex = False  
+  for cycle in cycles:
+    segments = hg19.interval_list([amplicon['segment_map'][s[0:-1]] for s in amplicon['cycle_map'][cycle]['cycle'] if s[0:-1] != '0'])
+    segments.sort()
+    hits = hg19.interval_list(Set([h[0] for h in intervals.intersection(segments)]))
+    hits.sort()
+    chrs = Set([s.chrom for s in segments])
+    if len(chrs) > 1 or len(hits) > 1:
+      iscomplex = True
+    #Sometimes the length is 0, not sure why      
+    length = sum([s.end - s.start for s in segments])
+    if length == 0:
+      continue
+    copies = sum([(s.end - s.start)*s.info['copy_count'] for s in segments])/length
+    #Check for cycles
+    if amplicon['cycle_map'][cycle]['cycle'][0][0] != '0':      
+      length = sum([s.end - s.start for s in segments])
+      #check is coverage across cycle is greater than threshold 
+      copies = sum([(s.end - s.start)*s.info['copy_count'] for s in segments])/length
+      if copies < threshold_copy or length < min_length:
+        continue
+      iscycle = True
+  if iscycle:
+    return 'Cyclic'
+  elif iscomplex:
+    return 'Complex'
+  else:
+    return 'Amplification'
+
+#Loads all amplicons - in_dir: directory containing $prefix_summary.txt file
+#                      prefix: prefix of the file
+def load_aa_result(in_dir, prefix):
+  summary_file = "%s/%s_summary.txt" % (in_dir, prefix)
+  all_map = {}
+  if os.path.exists(summary_file):    
+    amps = read_summary_file(summary_file)
+    if amps is None:
+      return None
+  else:
+    return None
+  for (ai, a) in amps.items():
+    ints = a['Intervals'].split(',')
+    cycle_file = "%s/%s_amplicon%s_cycles.txt" % (in_dir,prefix,ai)
+    (segment_map, interval_map, cycle_map) = read_cycle_file(cycle_file)
+    all_map.setdefault(ai,{})['segment_map'] = segment_map
+    all_map.setdefault(ai,{})['interval_map'] = interval_map
+    all_map.setdefault(ai,{})['cycle_map'] = cycle_map
+    for cycle in cycle_map.keys():
+      segments = hg19.interval_list([segment_map[c[0:-1]] for c in cycle_map[cycle]['cycle'] if c[0] != '0'])
+      for s in segments:
+        s.info.setdefault('copy_count',0)
+        s.info['copy_count']+=cycle_map[cycle]['copy_count']
+  all_map['amplicons'] = amps
+  return all_map
+
 
 #Reads a summary file
-def read_summary_file(summary_file):
+def read_summary_file(summary_file, cnv_file=None):
   input = open(summary_file,'r')
   amplicons = {}
   line = next(input, None)
   if line is None:
     return None
+  if int(line.split(' ')[-1].strip()) == 0:
+    return amplicons
   line = next(input, None)
   if line is None:
     return None
@@ -20,10 +89,11 @@ def read_summary_file(summary_file):
       id = int(res[3])
       amplicons.setdefault(id,{})      
     else:
-      amplicons[id].setdefault(res[1],res[3])
+      amplicons[id].setdefault(res[1],res[3])  
   if len(amplicons[id].keys()) < 5:
     return None
   return amplicons
+
 
 #Reads a cycle file
 def read_cycle_file(cycle_file):
@@ -51,24 +121,6 @@ def read_cycle_file(cycle_file):
   input.close()
   return (segment_map, interval_map, cycle_map)    
 
-#Loads the summary file and amplicons for an AA run
-#prefix is the prefix of the summary/amplicon files and in_dir is the
-#directory of the AA results
-def load_aa_result(prefix, in_dir):
-  summary_file = "%s/%s_summary.txt" % (in_dir, prefix)
-  all_map = {}
-  amps = read_summary_file(summary_file)
-  
-  for (ai, a) in amps.items():
-    ints = a['Intervals'].split(',')
-    cycle_file = "%s/%s_amplicon%s_cycles.txt" % (in_dir,prefix,ai)
-    (segment_map, interval_map, cycle_map) = read_cycle_file(cycle_file)
-    all_map.setdefault('amplicons',{}).setdefault(ai,{})['segment_map'] = segment_map
-    all_map.setdefault('amplicons',{}).setdefault(ai,{})['interval_map'] = interval_map
-    all_map.setdefault('amplicons',{}).setdefault(ai,{})['cycle_map'] = cycle_map
-  all_map['summary'] = amps
-  return all_map
-
 #Returns the cyclic paths of an amplicon of at least 10kb
 def get_cyclic_path(amplicon, threshold = 10000):
   paths = hg19.interval_list()
@@ -86,8 +138,8 @@ def valid_cycle(amplicon, threshold = 10000):
     is_cyclic = is_cyclic or (amplicon['cycle_map'][cycle]['cycle'][0][0] != '0' and length >= threshold)      
   return is_cyclic
 
-aa_result = load_aa_result(in_dir='/pedigree2/projects/namphuon/programs/pancancer/analyses/final/TUMOR-WGS-GWSNP6NOCNV/RAW/TCGA-06-0125-01A-01_WGS335134030397_CNV19272_mincnv1_20170905-133947/output', prefix='TCGA-06-0125-01A-01_WGS335134030397_CNV19272-MINCNV1')
+aa_result = load_aa_result(in_dir='/pedigree2/projects/namphuon/programs/pancancer/analyses/final/TUMOR-WGS-GWSNP6NOCNV/RAW/TCGA-06-0125-01A-01_WGS335134030397_CNV19272_mincnv1_20170905-133947/output', prefix='TCGA-06-0125-01A-01_WGS335134030397_CNV19272')
 
-#Now find which amplicons have a cyclic cycle
-for amplicon in aa_result['amplicons'].keys():
-  print "%d %s" % (amplicon, valid_cycle(aa_result['amplicons'][amplicon]))
+#Now classify each amplicon
+for amplicon in [a for a in aa_result.keys() if a != 'amplicons']:
+  print "%d %s" % (amplicon, classify_amplicon(aa_result[amplicon]))
